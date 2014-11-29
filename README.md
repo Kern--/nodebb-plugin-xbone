ab2a8890ffa7b43666c6d3183450b1e02d91eddd
Kern <wals0323@umn.edu>
2014-11-29 00:19:24 -0500

    Fix everything

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ NodeBB Plugin XBone
 -------------------
 Does it bother you when people incorrectly refer to Microsoft's newest gaming console as the "Xbox One?"
 
-Well, worry no more! This plugin will automatically correct users' posts to the proper term: "XBone"
+Well, worry no more! This plugin will automatically correct users' posts to the proper term: "Xbone"
 
 __install__
 `npm install nodebb-plugin-xbone`
--- library.js
+++ library.js
@@ -1,6 +1,24 @@
+"use strict";
+
 var Xbone = {
-    xbonify: function(data, callback) {
-        data.postData = data.postData.replace(/[Xx]box\s?[Oo]ne/, "xbone");
+    xbonify: function(str) {
+        return str
+            .replace(/xbox\s*one/gi, "Xbone")
+            .replace(/xb\s+one/gi, "Xbone");
+    },
+    xbonifyRaw: function(content, callback) {
+        callback(null, content ? Xbone.xbonify(content) : content);
+    },
+    xbonifyPost: function(data, callback) {
+        if (data && data.postData && data.postData.content) {
+            data.postData.content = Xbone.xbonify(data.postData.content);
+        }
+        callback(null, data);
+    },
+    xbonifySignature: function(data, callback) {
+        if (data && data.userData && data.userData.signature) {
+            data.userData.signature = Xbone.xbonify(data.userData.signature);
+        }
         callback(null, data);
     }
 }
